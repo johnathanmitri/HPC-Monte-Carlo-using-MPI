b22818e7aa23fad1db7633abb84ebc463c8b773c
Johnathan Mitri <jmitri@commander.unx.cpp.edu>
2024-02-22 01:34:21 -0800

    Created Compile script and added basic test code

--- compile_monty_pi.sh
+++ compile_monty_pi.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-
-srun -p compute --pty /usr/bin/bash
-
-module load openmpi/2.1.2
-
-g++ -o pi_mpi monti_pi.cpp -I /opt/openmpi-2.1.2/include -L /opt/openmpi-2.1.2/lib -l mpi
+srun -p compute /usr/bin/bash -c '
+source ~/.bashrc &&
+module load openmpi/2.1.2 &&
+g++ -o pi_mpi monty_pi.cpp -I /opt/openmpi-2.1.2/include -L /opt/openmpi-2.1.2/lib -l mpi 
+'
--- monty_pi.cpp
+++ monty_pi.cpp
@@ -1,17 +1,21 @@
 #include <math.h>
 #include <stdlib.h>
 #include <iostream>
+#include "mpi.h"
 
-int main(int argc, char const *argv[])
+int main(int argc, char **argv)
 {
-    
-    int myid = 7;
-    srand((unsigned)(myid));
+    int myid;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &myid);
+    std::cout << "My id is: " << myid << std::endl;
+    srand((unsigned)(myid*time(0)));
 
-    int x = ((double)rand()) / ((double)RAND_MAX);
-    int y = ((double)rand()) / ((double)RAND_MAX);
+    double x = ((double)rand()) / ((double)RAND_MAX);
+    double y = ((double)rand()) / ((double)RAND_MAX);
 
     std::cout << x << " " << y << std::endl;
 
+    MPI_Finalize();
     return 0;
 }
